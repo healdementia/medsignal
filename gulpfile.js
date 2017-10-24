8e49ba1a104bd61fcbfcaae049fba20fb0d66da6
Marina Kuznetsova <infans.autumnale@gmail.com>
2017-10-24 19:51:27 +0400

    Верстает верхнюю часть страницы

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var autoprefixer = require("autoprefixer");
+var postcss = require("gulp-postcss");
+var server = require("browser-sync").create();
+
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
